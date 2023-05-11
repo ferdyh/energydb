6a52a91564faa2a4b59937c4d946fcb874e6c909
Ferdy Hulzebos <ferdy.hulzebos@gmail.com>
2023-05-11 15:01:01 +0200

    Added CORS for VUE project

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Database
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 db = Database()
 
-app.mount("/web", StaticFiles(directory="./web"), name="web")
+app.mount("/web", StaticFiles(directory="./web/dist/"), name="web")
 
 @app.get("/")
 def root():
